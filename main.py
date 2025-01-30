0814b66f49a8f3ba41fd919a1751ddc740c103f9
ferbcn <ferbcn@pm.me>
2025-01-30 14:27:21 +0100

    cpu

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 from transformers import pipeline
-from transformers import AutoTokenizer
 
 
 def generate_text():
-    # # Using an open-source model instead of gated Llama model
-    # model_id = "facebook/opt-350m"
-    # pipe = pipeline(
-    #     "text-generation",
-    #     model=model_id,
-    #     device_map="auto",
-    # )
-    # messages = [
-    #     {"role": "system", "content": "You are a helpful assistant."},
-    #     {"role": "user", "content": "Should I let my dog sleep on the bed with me?"},
-    # ]
-    # tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
-    # tokenizer.apply_chat_template(messages, tokenize=False)
-    #
-    # outputs = pipe(
-    #     tokenizer,
-    #     max_new_tokens=128,
-    #     do_sample=True
-    # )
-    # print(outputs[0]["generated_text"])
-    pipe = pipeline("text-generation", "HuggingFaceH4/zephyr-7b-beta", device="gpu")
+    pipe = pipeline("text-generation", "HuggingFaceH4/zephyr-7b-beta", device="cpu")
     messages = [
         {
             "role": "system",
