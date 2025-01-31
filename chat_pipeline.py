c354257d3ccb8393fc7df78748cf49b1add2a943
ferbcn <ferbcn@pm.me>
2025-01-31 09:19:11 +0100

    fine tune, synth data

--- chat_pipeline.py
+++ chat_pipeline.py
@@ -1,6 +1,8 @@
 import os
 from transformers import pipeline
 from dotenv import load_dotenv
+import sys
+
 
 load_dotenv()
 
@@ -11,18 +13,22 @@ TOKEN = os.getenv('ACCESS_TOKEN')
 model_name="meta-llama/Llama-3.2-1B-Instruct"
 
 
-def generate_text():
+def generate_text(prompt):
     pipe = pipeline("text-generation", model_name, device="cuda", token=TOKEN)
     messages = [
         {
             "role": "system",
             "content": "You are a friendly chatbot who always responds in the style of a pirate",
         },
-        {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
+        {"role": "user", "content": prompt},
     ]
     print(pipe(messages, max_new_tokens=128)[0]['generated_text'][-1])  # Print the assistant's response
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    generate_text()
+    command_line = sys.argv[1:]
+    prompt = " ".join(command_line)
+    if len(prompt) < 5:
+        prompt = input("Please enter a prompt:")
+    generate_text(prompt)
