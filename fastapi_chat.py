2d6cb732f3536ef0949aeec5600d0cc140b00b67
ferbcn <ferbcn@pm.me>
2025-02-03 15:20:54 +0100

    deepseek eot

--- fastapi_chat.py
+++ fastapi_chat.py
@@ -99,7 +99,7 @@ def stream_text(prompt, model_name, max_tokens=256, temp=0.7, top_p=0.95):
         # print(new_token)
         # For fine-tuned model, look for the end of the assistant's response
         # Check for various end tokens and clean up the text
-        if tok.eos_token_id in tok(new_token)['input_ids'] or "<|eot_id|>" in new_token:
+        if tok.eos_token_id in tok(new_token)['input_ids'] or "<|eot_id|>" in new_token or "<｜end▁of▁sentence｜>" in new_token:
             clean_token = new_token.split("<|eot_id|>")[0].strip()
             big_chunk += clean_token
             yield big_chunk
