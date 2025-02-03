f9f93d136460fb0f1cdf1bc46f5c3c6a2fd6b7ff
ferbcn <ferbcn@pm.me>
2025-02-03 15:12:44 +0100

    model

--- chat_pipeline.py
+++ chat_pipeline.py
@@ -13,8 +13,8 @@ TOKEN = os.getenv('ACCESS_TOKEN')
 # model_name = "meta-llama/Llama-3.2-1B-Instruct"
 # model_name = "mistralai/Mistral-7B-Instruct-v0.3"
 # model_name = "HuggingFaceH4/zephyr-7b-beta"
-model_name = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
-
+# model_name = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 
 def generate_text(prompt):
     pipe = pipeline("text-generation", model_name, device="cuda", token=TOKEN)
